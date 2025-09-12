356f1ae63ae39867fd81186ce5cabcd73e29e23f
1499271434 <1499271434@qq.com>
2025-09-11 22:18:10 -0400

    添加API

--- js/customer_site.js
+++ js/customer_site.js
@@ -2,6 +2,14 @@ const CUSTOMER_SITES = {
     qiqi: {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
+    },
+    360: {
+        api: 'https://360zyzz.com/api.php/provide/vod',
+        name: '360资源',
+    },
+    ikresource: {
+        api: 'https://ikunzyapi.com/api.php/provide/vod',
+        name: 'IK资源',
     }
 };
 
